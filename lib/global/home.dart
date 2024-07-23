4edb781c17857baf46816268f8660b693e63d242
ElenaReyes19 <84975845+ElenaReyes19@users.noreply.github.com>
2024-07-23 10:37:35 -0600

    Actualizar home.dart

--- lib/global/home.dart
+++ lib/global/home.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(SpotifyHome());
+  runApp(MelonMixHome());
 }
 
-class SpotifyHome extends StatelessWidget {
+class MelonMixHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
