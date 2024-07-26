55309dc5daa6edf8bbe89857fc4e402b0d7f2d43
CesarGonzalezS <20213tn065@utez.edu.mx>
2024-07-26 11:50:17 -0600

    cambios para mejorar
    xd

--- lib/view/register/RegisterScreen.dart
+++ lib/view/register/RegisterScreen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:melomix/global/helpers/apis/api_service_loggin.dart';  // Asegúrate de usar la ruta correcta
-import 'package:melomix/view/splash_login.dart'; // Asegúrate de usar la ruta correcta
+import 'package:intl/intl.dart';
 
 class RegisterScreen extends StatefulWidget {
   @override
@@ -9,256 +8,98 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> {
   final _formKey = GlobalKey<FormState>();
-  String _email = '';
-  String _password = '';
-  String _confirmPassword = '';
-  bool _isLoading = false;
-  String _errorMessage = '';
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _dateController = TextEditingController();
 
-  final ApiService _apiService = ApiService();
-
-  // Método para mostrar un diálogo de alerta
-  void _showAlert(String title, String message) {
-    showDialog(
+  void _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
       context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          backgroundColor: Colors.white, // Fondo del diálogo
-          title: Text(
-            title,
-            style: TextStyle(color: Colors.black), // Color del título
-          ),
-          content: Text(
-            message,
-            style: TextStyle(color: Colors.black), // Color del mensaje
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text(
-                'OK',
-                style: TextStyle(color: Colors.blue), // Color del botón
-              ),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
     );
-  }
-
-  void _register() async {
-    if (_formKey.currentState!.validate()) {
-      if (_password != _confirmPassword) {
-        _showAlert('Error', 'Las contraseñas no coinciden.');
-        return;
-      }
-
+    if (picked != null) {
       setState(() {
-        _isLoading = true;
-        _errorMessage = '';
+        _dateController.text = DateFormat('yyyy-MM-dd').format(picked);
       });
-
-      try {
-        final success = await _apiService.register(_email, _password);
-
-        if (success) {
-          // Navegar de vuelta a la pantalla de inicio de sesión si el registro es exitoso
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => SplashView()),
-          );
-        } else {
-          _showAlert('Error', 'El registro falló. Inténtalo de nuevo.');
-        }
-      } catch (e) {
-        _showAlert('Error', 'Hubo un problema con la conexión. Inténtalo de nuevo.');
-      } finally {
-        setState(() {
-          _isLoading = false;
-        });
-      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // Logo de bienvenida con animación
-              AnimatedLogo(),
-              SizedBox(height: 10),
-              // Texto de bienvenida
-              Text(
-                'Registro de Usuario',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+      appBar: AppBar(
+        title: Text('Registrarse'),
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _usernameController,
+                  decoration: InputDecoration(labelText: 'Username'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a username';
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              SizedBox(height: 5),
-              // Contenedor para el formulario de registro
-              Container(
-                constraints: BoxConstraints(maxWidth: 600, minHeight: 300), // Controla el ancho máximo del contenedor
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.1),
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.5),
-                      spreadRadius: 5,
-                      blurRadius: 15,
-                      offset: Offset(0, 3),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(labelText: 'Email'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter an email';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(labelText: 'Password'),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _dateController,
+                  decoration: InputDecoration(
+                    labelText: 'Date Joined',
+                    suffixIcon: IconButton(
+                      icon: Icon(Icons.calendar_today),
+                      onPressed: () => _selectDate(context),
                     ),
-                  ],
-                ),
-                padding: EdgeInsets.all(20),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      // Campo de texto para el email
-                      TextFormField(
-                        style: TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                          labelText: 'Correo electrónico',
-                          labelStyle: TextStyle(color: Colors.white),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                        ),
-                        keyboardType: TextInputType.emailAddress,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Por favor ingrese su correo electrónico';
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          setState(() {
-                            _email = value;
-                          });
-                        },
-                      ),
-                      SizedBox(height: 10),
-                      // Campo de texto para la contraseña
-                      TextFormField(
-                        style: TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                          labelText: 'Contraseña',
-                          labelStyle: TextStyle(color: Colors.white),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                        ),
-                        obscureText: true,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Por favor ingrese su contraseña';
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          setState(() {
-                            _password = value;
-                          });
-                        },
-                      ),
-                      SizedBox(height: 10),
-                      // Campo de texto para confirmar la contraseña
-                      TextFormField(
-                        style: TextStyle(color: Colors.white),
-                        decoration: InputDecoration(
-                          labelText: 'Confirmar Contraseña',
-                          labelStyle: TextStyle(color: Colors.white),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.green),
-                          ),
-                        ),
-                        obscureText: true,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Por favor confirme su contraseña';
-                          }
-                          return null;
-                        },
-                        onChanged: (value) {
-                          setState(() {
-                            _confirmPassword = value;
-                          });
-                        },
-                      ),
-                      SizedBox(height: 20),
-                      // Botón de registro
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.green, // Color del botón
-                          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                          textStyle: TextStyle(fontSize: 18),
-                        ),
-                        onPressed: _register,
-                        child: _isLoading
-                            ? CircularProgressIndicator() // Mostrar indicador de carga mientras se realiza el registro
-                            : Text('Registrarse'),
-                      ),
-                      SizedBox(height: 10),
-                      // Mensaje de error
-                      if (_errorMessage.isNotEmpty)
-                        Text(
-                          _errorMessage,
-                          textAlign: TextAlign.center,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          softWrap: true, // Ajustar el texto en caso de que sea demasiado largo para la pantalla
-                          style: TextStyle(color: Colors.red, fontSize: 16),
-                        ),
-                      SizedBox(height: 10),
-                      // Texto y botón para volver a iniciar sesión
-                      Text(
-                        '¿Ya tienes una cuenta?',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(builder: (context) => SplashView()),
-                          );
-                        },
-                        child: Text(
-                          'Iniciar sesión',
-                          style: TextStyle(
-                            color: Colors.green,
-                            fontSize: 18,
-                            decoration: TextDecoration.underline,
-                          ),
-                        ),
-                      ),
-                    ],
                   ),
+                  readOnly: true,
                 ),
-              ),
-            ],
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() ?? false) {
+                      // Aquí puedes añadir la lógica para registrar al usuario
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  child: Text('Registrar'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
