a5c47c603447b32d48e8be18a6ab54666006ed02
ElenaReyes19 <20203tn003@utez.edu.mx>
2024-08-12 01:31:18 -0600

    cambios de integracion, crud, consumo

--- lib/view/main_tabview/main_tabview.dart
+++ lib/view/main_tabview/main_tabview.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:melomix/view/splash_login.dart';
-
 import 'package:get/get.dart'; // Asegúrate de importar GetX
 import 'package:melomix/common/color_extension.dart';
 import 'package:melomix/common_widget/icon_text_row.dart';
@@ -38,127 +37,229 @@ class _MainTabViewState extends State<MainTabView> with SingleTickerProviderStat
     super.dispose();
   }
 
+  void _onPlayPause() {
+    // Implementa la lógica de play/pause
+  }
+
+  void _onSkipBack() {
+    // Implementa la lógica de skip back
+  }
+
+  void _onSkipForward() {
+    // Implementa la lógica de skip forward
+  }
+
+  void _onShuffle() {
+    // Implementa la lógica de shuffle
+  }
+
+  void _onRepeat() {
+    // Implementa la lógica de repeat
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
+      body: Stack(
         children: [
-          AnimatedContainer(
-            duration: const Duration(milliseconds: 300),
-            width: _isMenuExpanded ? 250.0 : 70.0, // Ancho del menú
-            color: const Color(0xff10121D), // Color de fondo del menú
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                IconButton(
-                  icon: Icon(
-                    _isMenuExpanded ? Icons.arrow_back : Icons.menu,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _isMenuExpanded = !_isMenuExpanded;
-                    });
-                  },
-                ),
-           
-                IconTextRow(
-                  title: "Driver Mode",
-                  icon: "assets/img/img3.jpg",
-                  onTap: () {},
-                  isExpanded: _isMenuExpanded,
-                  
-                ),
-                IconTextRow(
-                  title: "Hidden Folders",
-                  icon: "assets/img/img2.jpg",
-                  onTap: () {},
-                  isExpanded: _isMenuExpanded,
-                ),
-                IconTextRow(
-                  title: "Scan Media",
-                  icon: "assets/img/img1.jpg",
-                  onTap: () {},
-                  isExpanded: _isMenuExpanded,
-                ),
-                ListTile(
-                  leading: Icon(Icons.login, color: Colors.white),
-                  title: _isMenuExpanded
-                      ? Text('Register', style: TextStyle(color: Colors.white))
-                      : null,
-                  onTap: () {
-                    Get.toNamed(AppRoutes.register); // Navega a la pantalla de registro
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.person_add, color: Colors.white),
-                  title: _isMenuExpanded
-                      ? Text('Register', style: TextStyle(color: Colors.white))
-                      : null,
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SplashView()), // Navega a SplashView
-                    );
-                  },
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Scaffold(
-              body: Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  Column(
-                    children: [
-          
-                      Expanded(
-                        child: TabBarView(
-                          controller: controller,
-                          children: [
-                            HomeView(),
-                            Container(), // Placeholder for Search view
-                            Container(), // Placeholder for Library view
-                          ],
-                        ),
+          Row(
+            children: [
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                width: _isMenuExpanded ? 250.0 : 70.0, // Ancho del menú
+                color: const Color(0xff10121D), // Color de fondo del menú
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      icon: Icon(
+                        _isMenuExpanded ? Icons.arrow_back : Icons.menu,
+                        color: Colors.white,
                       ),
+                      onPressed: () {
+                        setState(() {
+                          _isMenuExpanded = !_isMenuExpanded;
+                        });
+                      },
+                    ),
+                    IconTextRow(
+                      title: "Driver Mode",
+                      icon: "assets/img/img3.jpg",
+                      onTap: () {},
+                      isExpanded: _isMenuExpanded,
+                    ),
+                    IconTextRow(
+                      title: "Hidden Folders",
+                      icon: "assets/img/img2.jpg",
+                      onTap: () {},
+                      isExpanded: _isMenuExpanded,
+                    ),
+                    IconTextRow(
+                      title: "Scan Media",
+                      icon: "assets/img/img1.jpg",
+                      onTap: () {},
+                      isExpanded: _isMenuExpanded,
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.login, color: Colors.white),
+                      title: _isMenuExpanded
+                          ? Text('Register', style: TextStyle(color: Colors.white))
+                          : null,
+                      onTap: () {
+                        Get.toNamed(AppRoutes.register); // Navega a la pantalla de registro
+                      },
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.person_add, color: Colors.white),
+                      title: _isMenuExpanded
+                          ? Text('Register', style: TextStyle(color: Colors.white))
+                          : null,
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SplashView()), // Navega a SplashView
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Scaffold(
+                  body: TabBarView(
+                    controller: controller,
+                    children: [
+                      HomeView(),
+                      Container(), // Placeholder for Search view
+                      Container(), // Placeholder for Library view
                     ],
                   ),
-                  MiniPlayerView(),
-                ],
+                ),
               ),
-              bottomNavigationBar: BottomNavigationBar(
-                items: const <BottomNavigationBarItem>[
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.home),
-                    label: 'Home',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.search),
-                    label: 'Search',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Icon(Icons.library_music),
-                    label: 'Library',
-                  ),
-                ],
-                currentIndex: selectTab,
-                selectedItemColor: Color.fromARGB(255, 160, 5, 5),
-                unselectedItemColor: Colors.white,
-                backgroundColor: Color.fromARGB(255, 15, 15, 15),
-                onTap: (index) {
-                  setState(() {
-                    selectTab = index;
-                    controller?.animateTo(index);
-                  });
-                },
-              ),
+            ],
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: PlayerControls(
+              onPlayPause: _onPlayPause,
+              onSkipBack: _onSkipBack,
+              onSkipForward: _onSkipForward,
+              onShuffle: _onShuffle,
+              onRepeat: _onRepeat,
+              currentPosition: Duration(seconds: 120), // Ejemplo de duración actual
+              totalDuration: Duration(minutes: 3), // Ejemplo de duración total
+              onSeek: (value) {
+                // Implementa la lógica para avanzar en la canción
+              },
             ),
           ),
         ],
       ),
     );
+  }
+}
+
+class PlayerControls extends StatelessWidget {
+  final Function() onPlayPause;
+  final Function() onSkipBack;
+  final Function() onSkipForward;
+  final Function() onShuffle;
+  final Function() onRepeat;
+  final Duration currentPosition;
+  final Duration totalDuration;
+  final ValueChanged<double> onSeek;
+
+  PlayerControls({
+    required this.onPlayPause,
+    required this.onSkipBack,
+    required this.onSkipForward,
+    required this.onShuffle,
+    required this.onRepeat,
+    required this.currentPosition,
+    required this.totalDuration,
+    required this.onSeek,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final double progress = currentPosition.inSeconds.toDouble() / totalDuration.inSeconds.toDouble();
+
+    return Container(
+      color: Color.fromARGB(255, 15, 15, 15),
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 1),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(Icons.shuffle, size: 16, color: Colors.white),
+                onPressed: onShuffle,
+              ),
+              IconButton(
+                icon: Icon(Icons.skip_previous, size: 16, color: Colors.white),
+                onPressed: onSkipBack,
+              ),
+              IconButton(
+                icon: Icon(Icons.play_arrow, size: 20, color: Colors.white), // Cambia a play/pause según el estado
+                onPressed: onPlayPause,
+              ),
+              IconButton(
+                icon: Icon(Icons.skip_next, size: 16, color: Colors.white),
+                onPressed: onSkipForward,
+              ),
+              IconButton(
+                icon: Icon(Icons.repeat, size: 16, color: Colors.white),
+                onPressed: onRepeat,
+              ),
+            ],
+          ),
+          SizedBox(height: 3), // Espacio entre los botones y el Slider
+          Center(
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      _formatDuration(currentPosition),
+                      style: TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                    SizedBox(
+                      width: 450, // Ajusta el ancho del Slider
+                      child: Container(
+                        height: 1, // Ajusta la altura del Slider
+                        child: Slider(
+                          value: progress,
+                          onChanged: onSeek,
+                          activeColor: Colors.white,
+                          inactiveColor: Colors.grey,
+                          min: 0,
+                          max: 1,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      _formatDuration(totalDuration),
+                      style: TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String _formatDuration(Duration duration) {
+    final minutes = duration.inMinutes;
+    final seconds = duration.inSeconds % 60;
+    return '${minutes}:${seconds.toString().padLeft(2, '0')}';
   }
 }
 
@@ -178,7 +279,7 @@ class IconTextRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Image.asset(icon, width: 24, color: Colors.white),
+      leading: Image.asset(icon, width: 20, color: Colors.white),
       title: isExpanded ? Text(title, style: TextStyle(color: Colors.white)) : null,
       onTap: onTap,
     );
