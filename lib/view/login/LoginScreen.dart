1bc7c3e10da1bcdc6b3aa8937568bf6aa1807192
emilianovargas13 <20203tn061@utez.edu.mx>
2024-08-15 13:54:43 -0600

    Diseño de LOGIN
    Cambiamos diseño del login

--- lib/view/login/LoginScreen.dart
+++ lib/view/login/LoginScreen.dart
@@ -14,100 +14,197 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  final ApiServices _apiServices = ApiServices(); // Instancia del servicio de API
+  final ApiServices _apiServices = ApiServices();
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Iniciar Sesión'),
-        leading: IconButton(
-          icon: Icon(Icons.close),
-          onPressed: () {
-            Get.back();
-          },
-        ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                TextFormField(
-                  controller: _usernameController,
-                  decoration: InputDecoration(labelText: 'Username'),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor ingrese un nombre de usuario';
-                    }
-                    return null;
-                  },
+      backgroundColor: Colors.black,
+      body: Center(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'MelonMix',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(labelText: 'Password'),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Por favor ingrese una contraseña';
-                    }
-                    return null;
-                  },
+              ),
+              SizedBox(height: 30),
+              Card(
+                color: Colors.grey[850],
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
                 ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState?.validate() ?? false) {
-                      String username = _usernameController.text.trim();
-                      String password = _passwordController.text.trim();
-
-                      print('Credenciales ingresadas: username=$username, password=$password');
-
-                      bool loginSuccess = await _apiServices.loginUser(username, password);
-
-                      if (loginSuccess) {
-                        print('Inicio de sesión exitoso');
-
-                        // Verifica el grupo de usuario y redirige a la ruta correspondiente
-                        final prefs = await SharedPreferences.getInstance();
-                        String? userGroup = prefs.getString('user_group');
-
-                        if (userGroup == 'admin') {
-                          Get.toNamed(AppRoutes.admin);
-                        } else {
-                          Get.toNamed(AppRoutes.home);
-                        }
-                      } else {
-                        print('Credenciales incorrectas');
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(content: Text('Inicio de sesión fallido, revise sus credenciales')),
-                        );
-                      }
-                    }
-                  },
-                  child: Text('Iniciar Sesión'),
+                elevation: 10,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ConstrainedBox(
+                          constraints: BoxConstraints(maxWidth: 350), // Ancho ligeramente mayor para los inputs
+                          child: _buildUsernameField(),
+                        ),
+                        SizedBox(height: 15),
+                        ConstrainedBox(
+                          constraints: BoxConstraints(maxWidth: 350),
+                          child: _buildPasswordField(),
+                        ),
+                        SizedBox(height: 20),
+                        ConstrainedBox(
+                          constraints: BoxConstraints(maxWidth: 250), // Ancho ligeramente mayor para el botón
+                          child: _buildLoginButton(),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {
-                    Get.toNamed(AppRoutes.home);
-                  },
-                  child: Text('Ir al Home'),
-                ),
-                SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {
-                    Get.toNamed(AppRoutes.register); // Navega a la pantalla de registro
-                  },
-                  child: Text('¿No tienes una cuenta? Regístrate'),
-                ),
-              ],
-            ),
+              ),
+              SizedBox(height: 20),
+              _buildAdditionalOptions(),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildUsernameField() {
+    return TextFormField(
+      controller: _usernameController,
+      decoration: InputDecoration(
+        labelText: 'Username',
+        labelStyle: TextStyle(color: Colors.grey[400]),
+        prefixIcon: Icon(Icons.person, color: Colors.white),
+        filled: true,
+        fillColor: Colors.grey[800],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide.none,
+        ),
+      ),
+      style: TextStyle(color: Colors.white),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Por favor ingrese un nombre de usuario';
+        }
+        return null;
+      },
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      textInputAction: TextInputAction.next,
+      autofocus: true,
+    );
+  }
+
+  Widget _buildPasswordField() {
+    return TextFormField(
+      controller: _passwordController,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        labelStyle: TextStyle(color: Colors.grey[400]),
+        prefixIcon: Icon(Icons.lock, color: Colors.white),
+        filled: true,
+        fillColor: Colors.grey[800],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide.none,
+        ),
+      ),
+      style: TextStyle(color: Colors.white),
+      obscureText: true,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Por favor ingrese una contraseña';
+        }
+        return null;
+      },
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      textInputAction: TextInputAction.done,
+    );
+  }
+
+  Widget _buildLoginButton() {
+    return _isLoading
+        ? Center(child: CircularProgressIndicator())
+        : ElevatedButton(
+            onPressed: _login,
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 60),
+              backgroundColor: Colors.grey[700],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            child: Text(
+              'Iniciar Sesión',
+              style: TextStyle(fontSize: 20, color: Colors.white,),
+            ),
+          );
+  }
+
+  Widget _buildAdditionalOptions() {
+    return Column(
+      children: [
+        TextButton(
+          onPressed: () {
+            Get.toNamed(AppRoutes.home);
+          },
+          child: Text(
+            'Ir al Home',
+            style: TextStyle(color: Colors.grey[400]),
+          ),
+        ),
+        SizedBox(height: 10),
+        TextButton(
+          onPressed: () {
+            Get.toNamed(AppRoutes.register);
+          },
+          child: Text(
+            '¿No tienes una cuenta? Regístrate',
+            style: TextStyle(color: Colors.grey[400]),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _login() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      setState(() {
+        _isLoading = true;
+      });
+
+      String username = _usernameController.text.trim();
+      String password = _passwordController.text.trim();
+
+      bool loginSuccess = await _apiServices.loginUser(username, password);
+
+      setState(() {
+        _isLoading = false;
+      });
+
+      if (loginSuccess) {
+        final prefs = await SharedPreferences.getInstance();
+        String? userGroup = prefs.getString('user_group');
+
+        if (userGroup == 'admin') {
+          Get.toNamed(AppRoutes.admin);
+        } else {
+          Get.toNamed(AppRoutes.home);
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Inicio de sesión fallido, revise sus credenciales')),
+        );
+      }
+    }
   }
 }
