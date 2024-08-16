665aa3a2637f8b75c6f6b1fa51ecd2ed620019d6
emilianovargas13 <20203tn061@utez.edu.mx>
2024-08-16 00:17:13 -0600

    CRUD Songs Completo (SV)
    Crud funcionando, diseño implementado

--- lib/view/admin/song/songs_screen.dart
+++ lib/view/admin/song/songs_screen.dart
@@ -4,9 +4,6 @@ import 'package:melomix/services/api_services.dart';
 import 'package:melomix/presentation/cubits/song_cubit.dart';
 import 'package:melomix/presentation/cubits/song_state.dart';
 import 'package:melomix/data/model/song_model.dart';
-import 'package:melomix/view/admin/song/add_song_screen.dart';
-import 'package:melomix/view/admin/song/edit_song_screen.dart';
-import 'package:melomix/view/admin/song/delete_song_screen.dart';
 
 class SongsScreen extends StatefulWidget {
   @override
@@ -29,70 +26,84 @@ class _SongsScreenState extends State<SongsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,  // Fondo negro
       appBar: AppBar(
-        title: Text('Songs'),
+        title: const Text('Songs'),
+        backgroundColor: Colors.grey[900],  // Color oscuro para AppBar
       ),
       body: BlocBuilder<SongCubit, SongState>(
         builder: (context, state) {
           if (state is SongLoading) {
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator(color: Colors.white));
           } else if (state is SongSuccess) {
             return ListView.builder(
               itemCount: state.songs.length,
               itemBuilder: (context, index) {
                 final song = state.songs[index];
-                return ListTile(
-                  title: Text(song.title),
-                  subtitle: Text('Genre: ${song.genre} | Duration: ${song.duration} seconds'),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () => _confirmDelete(song.songId!),
-                  ),
-                  onTap: () {
-                    _titleController.text = song.title;
-                    _durationController.text = song.duration.toString(); // Convertimos int a String
-                    _albumIdController.text = song.albumId?.toString() ?? '';
-                    _artistIdController.text = song.artistId.toString();
-                    _genreController.text = song.genre;
+                return Card(
+                  color: Colors.grey[850],  // Color oscuro para las tarjetas
+                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: ListTile(
+                    leading: const Icon(Icons.music_note, color: Colors.white),  // Ícono de música
+                    title: Text(
+                      song.title,
+                      style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      'Genre: ${song.genre} | Duration: ${song.duration} seconds',
+                      style: TextStyle(color: Colors.grey[400]),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete, color: Colors.redAccent),  // Ícono de eliminación en rojo
+                      onPressed: () => _confirmDelete(song.songId!),
+                    ),
+                    onTap: () {
+                      _titleController.text = song.title;
+                      _durationController.text = song.duration.toString(); // Convertimos int a String
+                      _albumIdController.text = song.albumId?.toString() ?? '';
+                      _artistIdController.text = song.artistId.toString();
+                      _genreController.text = song.genre;
 
-                    showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        title: Text('Edit Song'),
-                        content: _buildSongForm(),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              final updatedSong = Song(
-                                songId: song.songId,
-                                title: _titleController.text,
-                                duration: int.parse(_durationController.text), // Convertimos String a int
-                                albumId: _albumIdController.text.isEmpty
-                                    ? null
-                                    : int.tryParse(_albumIdController.text),
-                                artistId: int.parse(_artistIdController.text),
-                                genre: _genreController.text,
-                              );
-                              context.read<SongCubit>().updateSong(updatedSong);
-                              Navigator.of(context).pop();
-                            },
-                            child: Text('Update'),
-                          ),
-                          TextButton(
-                            onPressed: () => Navigator.of(context).pop(),
-                            child: Text('Cancel'),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          backgroundColor: Colors.grey[900],  // Fondo oscuro en el diálogo
+                          title: const Text('Edit Song', style: TextStyle(color: Colors.white)),
+                          content: _buildSongForm(),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                final updatedSong = Song(
+                                  songId: song.songId,
+                                  title: _titleController.text,
+                                  duration: int.parse(_durationController.text), // Convertimos String a int
+                                  albumId: _albumIdController.text.isEmpty
+                                      ? null
+                                      : int.tryParse(_albumIdController.text),
+                                  artistId: int.parse(_artistIdController.text),
+                                  genre: _genreController.text,
+                                );
+                                context.read<SongCubit>().updateSong(updatedSong);
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Update', style: TextStyle(color: Colors.white)),
+                            ),
+                            TextButton(
+                              onPressed: () => Navigator.of(context).pop(),
+                              child: const Text('Cancel', style: TextStyle(color: Colors.redAccent)),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
                 );
               },
             );
           } else if (state is SongError) {
-            return Center(child: Text('Failed to load songs: ${state.message}'));
+            return Center(child: Text('Failed to load songs: ${state.message}', style: const TextStyle(color: Colors.white)));
           }
-          return Center(child: Text('No songs available.'));
+          return const Center(child: Text('No songs available.', style: TextStyle(color: Colors.white)));
         },
       ),
       floatingActionButton: FloatingActionButton(
@@ -101,14 +112,15 @@ class _SongsScreenState extends State<SongsScreen> {
           showDialog(
             context: context,
             builder: (context) => AlertDialog(
-              title: Text('Create Song'),
+              backgroundColor: Colors.grey[900],  // Fondo oscuro en el diálogo
+              title: const Text('Create Song', style: TextStyle(color: Colors.white)),
               content: _buildSongForm(),
               actions: [
                 TextButton(
                   onPressed: () {
                     final newSong = Song(
                       title: _titleController.text,
-                      duration: int.parse(_durationController.text), // Convertimos String a int
+                      duration: int.parse(_durationController.text), 
                       albumId: _albumIdController.text.isEmpty
                           ? null
                           : int.tryParse(_albumIdController.text),
@@ -118,17 +130,18 @@ class _SongsScreenState extends State<SongsScreen> {
                     context.read<SongCubit>().createSong(newSong);
                     Navigator.of(context).pop();
                   },
-                  child: Text('Create'),
+                  child: const Text('Create', style: TextStyle(color: Colors.white)),
                 ),
                 TextButton(
                   onPressed: () => Navigator.of(context).pop(),
-                  child: Text('Cancel'),
+                  child: const Text('Cancel', style: TextStyle(color: Colors.redAccent)),
                 ),
               ],
             ),
           );
         },
-        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[850],
+        child: const Icon(Icons.add, color: Colors.white),
       ),
     );
   }
@@ -139,26 +152,76 @@ class _SongsScreenState extends State<SongsScreen> {
       children: [
         TextField(
           controller: _titleController,
-          decoration: InputDecoration(labelText: 'Title'),
+          decoration: const InputDecoration(
+            labelText: 'Title',
+            labelStyle: TextStyle(color: Colors.white),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+          ),
+          style: const TextStyle(color: Colors.white),
         ),
-        TextField(
+        TextFormField(
           controller: _durationController,
-          decoration: InputDecoration(labelText: 'Duration (seconds)'), // Añadido para claridad
-          keyboardType: TextInputType.number, // Asegura que solo números puedan ser ingresados
+          decoration: const InputDecoration(
+            labelText: 'Duration (seconds)',
+            labelStyle: TextStyle(color: Colors.white),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+          ),
+          keyboardType: TextInputType.number,
+          style: const TextStyle(color: Colors.white),
         ),
         TextField(
           controller: _albumIdController,
-          decoration: InputDecoration(labelText: 'Album ID (Optional)'),
+          decoration: const InputDecoration(
+            labelText: 'Album ID (Optional)',
+            labelStyle: TextStyle(color: Colors.white),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+          ),
           keyboardType: TextInputType.number,
+          style: const TextStyle(color: Colors.white),
         ),
         TextField(
           controller: _artistIdController,
-          decoration: InputDecoration(labelText: 'Artist ID'),
+          decoration: const InputDecoration(
+            labelText: 'Artist ID',
+            labelStyle: TextStyle(color: Colors.white),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+          ),
           keyboardType: TextInputType.number,
+          style: const TextStyle(color: Colors.white),
         ),
         TextField(
           controller: _genreController,
-          decoration: InputDecoration(labelText: 'Genre'),
+          decoration: const InputDecoration(
+            labelText: 'Genre',
+            labelStyle: TextStyle(color: Colors.white),
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+          ),
+          style: const TextStyle(color: Colors.white),
         ),
       ],
     );
@@ -176,19 +239,20 @@ class _SongsScreenState extends State<SongsScreen> {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
-        title: Text('Delete Song'),
-        content: Text('Are you sure you want to delete this song?'),
+        backgroundColor: Colors.grey[900],  // Fondo oscuro en el diálogo
+        title: const Text('Delete Song', style: TextStyle(color: Colors.white)),
+        content: const Text('Are you sure you want to delete this song?', style: TextStyle(color: Colors.white)),
         actions: [
           TextButton(
             onPressed: () {
               context.read<SongCubit>().deleteSong(songId);
               Navigator.of(context).pop();
             },
-            child: Text('Delete'),
+            child: const Text('Delete', style: TextStyle(color: Colors.redAccent)),
           ),
           TextButton(
             onPressed: () => Navigator.of(context).pop(),
-            child: Text('Cancel'),
+            child: const Text('Cancel', style: TextStyle(color: Colors.white)),
           ),
         ],
       ),
